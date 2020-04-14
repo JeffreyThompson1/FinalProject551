81b4d68752b250ee4adccc26e5b6bed4e6865a4a
Sebastian McGonigle <60111427+SebastianMcgonigle@users.noreply.github.com>
2020-04-14 17:07:19 -0600

    Map Layer Activated

---
+++ flaskrunner.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export FLASK_ENV=development
+export FLASK_APP=application.py
+export DATABASE_URL=postgres://nrzfbmbyrfuiiv:bcf70f112a5c78fda0451689f2228006e08b08f976780950d889a94fd2737e5b@ec2-3-223-21-106.compute-1.amazonaws.com:5432/dbl2cg6nng3q61
+echo ready to run
